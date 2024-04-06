b58a15932fd72562ef58d8f6dc4fc56813151ba5
Adam Lopez <adamf04@gmail.com>
2024-04-06 00:58:57 -0400

    Setting the mongo DB cluster up on my node project. Next will add some env vars for the connection string for mongoDB.

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async() => {
     try {
-        await mongoose.connect("url");
+        await mongoose.connect("mongodb+srv://adamf04:tJfVmWwBtTd6qpaD@cluster0.pwvt4un.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB Connected...");
     } catch (error) {
         console.log("Error connecting to MongoDB: ", error.message);
