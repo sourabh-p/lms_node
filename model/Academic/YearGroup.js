38fe9bc07721443d1da8c76475f8f023a9405965
Adam Lopez <adamf04@gmail.com>
2024-04-05 22:14:47 -0400

    Adding the Year Group term model schema

---
+++ model/Academic/YearGroup.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const yearGroupSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Admin",
+            required: true,
+        },
+        academicYear: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "AcademicYear",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+// Model
+const YearGroup = mongoose.model("YearGroup", yearGroupSchema);
+
+module.exports = YearGroup;
