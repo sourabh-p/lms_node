b492a19a058728005c8d14810c39ac193c4b1f8c
Adam Lopez <adamf04@gmail.com>
2024-04-06 01:25:57 -0400

    loading the environment variable and setting up the server requirement

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const http = require("http");
+require("dotenv").config();
 require("./config/dbConnect"); // Database connection
 
-const app = require("./app/app");
-
+const http = require("http");
+const app  = require("./app/app");
 const PORT = process.env.PORT  || 2020;
 
 // server
