e4040e071dd1114f7a0c3fc7897f085ba0186ed1
Adam Lopez <adamf04@gmail.com>
2024-04-07 21:18:02 -0400

    Using bcrypt, before the save fuction is called, I am hashing the users password before it is inserted into the database. I am also performing a call to verify the users password to ensure they are entering the correct password comparing the entered password with the users password.

--- model/Staff/Admin.js
+++ model/Staff/Admin.js
@@ -1,4 +1,4 @@
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 const mongoose    = require("mongoose");
 const adminSchema = new mongoose.Schema(
@@ -24,6 +24,22 @@ const adminSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Hash Password
+adminSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  // salt then hash the password
+  const salt    = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//verify Password
+adminSchema.methods.verifyPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 //model
 const Admin = mongoose.model("Admin", adminSchema);
