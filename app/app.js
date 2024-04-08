132ed2b0ffba7773e4836044be6b2ed7489c1bdd
Adam Lopez <adamf04@gmail.com>
2024-04-08 13:39:18 -0400

    refactoring the app.js to handle error handling via middleware npm package

--- app/app.js
+++ app/app.js
@@ -1,51 +1,26 @@
 const express     = require('express');
 const morgan      = require('morgan');
 const adminRouter = require('../routes/staff/adminRouter');
+const globalErrHandler = require('../middlewares/globalErrHandler');
 
 const app  = express(); //  create application instance of express
 
 /**
  * Middleware
  */
-app.use(morgan("dev"));
 app.use(express.json()); // pass incoming json data
-
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.originalUrl}`);
-    next();
-});
-
-let user = {
-    name: "john doe",
-    isAdmin: false,
-    isLogin: true,
-};
-
-const isLogin = (req, res, next) => {
-    if(user.isLogin){
-        next();
-    } else {
-        res.status(401).json({
-            msg: "Unauthorized",
-        });
-    }
-};
-
-const isAdmin = (req, res, next) => {
-    if(user.isAdmin) {
-        next();
-    } else {
-        res.status(401).json({
-            msg: 'You are not an administrator'
-        });
-    }
-};
-
-app.use(isLogin, isAdmin); // chaining multiple middleware
 
 /**
  * Routes
  */
-app.use('/api/v1/admins', adminRouter); // Admin middleware
+app.use("/api/v1/admins", adminRouter); // Admin middleware
+
+/**
+ * Error Middlewares
+ * 
+ * when express sees the following, it will return
+ * as error handled middleware
+ */
+app.use(globalErrHandler);
 
 module.exports = app;
