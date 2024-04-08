9df391a4b60b412854141b2720c170af075587ba
Adam Lopez <adamf04@gmail.com>
2024-04-08 12:06:09 -0400

    Chaining custom middlewares using express next() within the app.

--- app/app.js
+++ app/app.js
@@ -10,6 +10,39 @@ const app  = express(); //  create application instance of express
 app.use(morgan("dev"));
 app.use(express.json()); // pass incoming json data
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
+let user = {
+    name: "john doe",
+    isAdmin: false,
+    isLogin: true,
+};
+
+const isLogin = (req, res, next) => {
+    if(user.isLogin){
+        next();
+    } else {
+        res.status(401).json({
+            msg: "Unauthorized",
+        });
+    }
+};
+
+const isAdmin = (req, res, next) => {
+    if(user.isAdmin) {
+        next();
+    } else {
+        res.status(401).json({
+            msg: 'You are not an administrator'
+        });
+    }
+};
+
+app.use(isLogin, isAdmin); // chaining multiple middleware
+
 /**
  * Routes
  */
