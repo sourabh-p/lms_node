27fea83845e75be3198d85cf908d5d670bdb56e1
Adam Lopez <adamf04@gmail.com>
2024-04-10 14:21:28 -0400

    "Add validation for unique academic year name and fetch all academic years in academicYearCtrl.js"
    This commit adds validation to check if the academic year name already exists before creating a new one. It also includes a new endpoint to fetch all academic years.

--- controller/academics/academicYearCtrl.js
+++ controller/academics/academicYearCtrl.js
@@ -3,11 +3,38 @@ const AcademicYear = require("../../model/Academic/AcademicYear");
 const Admin        = require("../../model/Staff/Admin");
 
 exports.createAcademicYear = AsyncHandler(async (req, res) => {
-    const { name, fromYear, toYear, isCurrent, createdBy  } = req.body;
+    const { name, fromYear, toYear  } = req.body;
+    // cehck if the year exists
+    const academicYear = await AcademicYear.findOne({name});
+    if(academicYear){
+        throw new Error("Academic year already exists");
+    }
+    // create
+    const academicYearCreated = await AcademicYear.create({
+        name,
+        fromYear,
+        toYear,
+        createdBy: req.userAuth._id
+    })
+
     res.status(201).json({
         status: 'success',
         message: "Academic year created",
-        data: "",
-    })
+        data: academicYearCreated,
+    });
+
+});
+
+exports.getAcademicYears = AsyncHandler(async (req, res) => {
+    const academicYears = await AcademicYear.find();
+    if(academicYears){
+        throw new Error("Academic year already exists");
+    }
+
+    res.status(201).json({
+        status: 'success',
+        message: "Academic years fetched successfully",
+        data: academicYears,
+    });
 
 });
