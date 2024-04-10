f65fb0cb86de1106e527216ef63030209d9ceddb
Adam Lopez <adamf04@gmail.com>
2024-04-10 14:21:03 -0400

    "Add validation for unique academic year name and fetch all academic years in academicYearCtrl.js"
    This commit adds a check for the existence of an academic year with the same name before creating a new one. It also includes a new endpoint to fetch all academic years.

--- routes/academics/academicYear.js
+++ routes/academics/academicYear.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { createAcademicYear } = require("../../controller/academics/academicYearCtrl");
+const isAdmin = require("../../middlewares/isAdmin");
+const isLogin = require("../../middlewares/isLogin");
 
 const academicYearRouter = express.Router();
 
-academicYearRouter.post("/", createAcademicYear);
+academicYearRouter.post("/", isLogin, isAdmin, createAcademicYear);
 
 module.exports = academicYearRouter;
