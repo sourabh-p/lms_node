df070bb69a5178e143903ac1bd05cd5d6c38050d
Adam Lopez <adamf04@gmail.com>
2024-04-10 23:35:41 -0400

    Really cool feature of this language is the routing in express. You can chain different route methods like get, put, etc.

--- routes/academics/academicYear.js
+++ routes/academics/academicYear.js
@@ -2,12 +2,36 @@ const express = require("express");
 const { createAcademicYear, getAcademicYears, getAcademicYear, updateAcademicYear, deleteAcademicYear } = require("../../controller/academics/academicYearCtrl");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
+const { create } = require("../../model/Academic/AcademicYear");
 
 const academicYearRouter = express.Router();
 
-academicYearRouter.post("/", isLogin, isAdmin, createAcademicYear);
-academicYearRouter.get("/", isLogin, isAdmin, getAcademicYears);
-academicYearRouter.get("/:id", isLogin, isAdmin, getAcademicYear);
-academicYearRouter.put("/:id", isLogin, isAdmin, updateAcademicYear);
-academicYearRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYear);
+/**
+ * Below is an example of route chaining in express. 
+ * The commented code is how you would normally write
+ * routes, but when chaining, it would look and function
+ * as written.
+ * 
+ * old routes
+ */
+// academicYearRouter.post("/", isLogin, isAdmin, createAcademicYear);
+// academicYearRouter.get("/", isLogin, isAdmin, getAcademicYears);
+// academicYearRouter.get("/:id", isLogin, isAdmin, getAcademicYear);
+// academicYearRouter.put("/:id", isLogin, isAdmin, updateAcademicYear);
+// academicYearRouter.delete("/:id", isLogin, isAdmin, deleteAcademicYear);
+
+/**
+ * updated chained routes
+ */
+academicYearRouter
+  .route("/")
+  .post(isLogin, isAdmin, createAcademicYear)
+  .get(isLogin, isAdmin, getAcademicYear);
+
+academicYearRouter
+  .route("/:id")
+  .get(isLogin, isAdmin, getAcademicYear)
+  .put(isLogin, isAdmin, updateAcademicYear)
+  .delete(isLogin, isAdmin, deleteAcademicYear);
+
 module.exports = academicYearRouter;
