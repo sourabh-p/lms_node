2687641dc7e219a67d929d1792cce085fd4f7c62
Adam Lopez <adamf04@gmail.com>
2024-04-13 13:47:41 -0400

    Adding routes for teacher router logging in. Adding middleware checks as well.

--- routes/staff/teacherRouter.js
+++ routes/staff/teacherRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
-const { adminRegisterTeacher } = require("../../controller/staff/teachersCtrl");
+const { adminRegisterTeacher, loginTeacher, getAllTeachersAdmin } = require("../../controller/staff/teachersCtrl");
 const teachersRouter = express.Router();
 
 teachersRouter.post("/admin/register", isLogin, isAdmin, adminRegisterTeacher);
+teachersRouter.post("/login", loginTeacher);
+teachersRouter.get("/admin", isLogin, isAdmin, getAllTeachersAdmin);
 
 module.exports = teachersRouter;
