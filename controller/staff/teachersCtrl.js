d1083641452edb6e42be9cf3df7513f91783fc97
Adam Lopez <adamf04@gmail.com>
2024-04-13 13:46:41 -0400

    Creatiing the login and get all endpoint methods within the teachers controller. The login teacher checks if the password is matched for the teacher then generates a token based on the teacher id. This tokenization allows for teacher data to not be exposed until a query is made to MongoDB with the appropriate information.

--- controller/staff/teachersCtrl.js
+++ controller/staff/teachersCtrl.js
@@ -1,6 +1,7 @@
 const expressAsyncHandler = require("express-async-handler");
 const Teacher = require("../../model/Staff/Teacher");
 const { hashPassword, isPassMatched } = require("../../utils/helpers");
+const generateToken = require("../../utils/generateToken");
 
 /**
  * @description Admin Register Teacher
@@ -27,5 +28,47 @@ exports.adminRegisterTeacher = expressAsyncHandler(async (req, res) => {
         status: "success",
         message: "Teacher registered Successfuly",
         data: teacherCreated,
+    });
+});
+
+/**
+ * @description Login a Teacehr
+ * @route       POST /api/teachers/login
+ * @access      Public
+ */
+
+exports.loginTeacher = expressAsyncHandler(async  (req, res)=>{
+    const { email, password } = req.body;
+
+    //find the teacher user obj
+    const teacher = await Teacher.findOne({email});
+    if( !teacher ) {
+        return res.json({message: "Invalid login credentials"});
+    }
+    // verify the password
+    const isMatched = await isPassMatched(password, teacher?.password);
+    if ( !isMatched ) {
+        return res.json({message:"Invalid login credentials"});
+    } else {
+        res.status(200).json({
+            status: "success",
+            message: "Teacher logged in successfully",
+            data: generateToken(teacher?._id),
+        });
+    }
+});
+
+/**
+ * @description Get All Teachers
+ * @route       GET /api/v1/admin/teachers
+ * @access      Private admin only
+ */
+
+exports.getAllTeachersAdmin = expressAsyncHandler( async(req, res) => {
+    const teachers = await Teacher.find();
+    res.status(200).json({
+        status: "success",
+        message: "Teachers fetched successfully",
+        data: teachers,
     });
 });
