3225b3d9b7a17c953d7c768d42cd14b15021d31a
Adam Lopez <adamf04@gmail.com>
2024-04-16 13:59:34 -0400

    Adding routes for getting one and all exams

--- routes/academics/examRoutes.js
+++ routes/academics/examRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const isTeacher = require("../../middlewares/isTeacher");
 const isTeacherLogin = require("../../middlewares/isTeacherLogin");
-const { createExam } = require("../../controller/academics/examsCtrl");
+const { createExam, getExams, getExam } = require("../../controller/academics/examsCtrl");
 
 const examRouter = express.Router();
 
+/**
+ * observe the middleware being defined in the route method of this chained router.
+ */
 examRouter
-    .route("/")
-    .post(isTeacherLogin, isTeacher, createExam);
+    .route("/", isTeacherLogin, isTeacher)
+    .post(createExam)
+    .get(getExams);
     
+examRouter
+    .route("/:id", isTeacherLogin, isTeacher)
+    .get(getExam);
+
 module.exports=examRouter;
