ee6387ea7699dce8e3eaa1c17801c442839233f5
Adam Lopez <adamf04@gmail.com>
2024-04-15 23:10:32 -0400

    Sending the string of the program will make it easier to store for the time being.

--- model/Staff/Teacher.js
+++ model/Staff/Teacher.js
@@ -49,9 +49,7 @@ const teacherSchema = new mongoose.Schema(
       default: "teacher",
     },
     subject: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Subject",
-      // required: true,
+      type: String,
     },
     // when you are registered, teacher goes through approval stage
     applicationStatus: {
@@ -60,20 +58,14 @@ const teacherSchema = new mongoose.Schema(
       default: "pending",
     },
     program: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Program",
-      // required: true,
+      type: String,
     },
     // A teacher can teach in more than one class level
     classLevel: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "ClassLevel",
-      // required: true,
+      type: String,
     },
     academicYear: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "AcademicYear",
-      // required: true,
+      type: String
     },
     examsCreated: [
       {
@@ -87,9 +79,7 @@ const teacherSchema = new mongoose.Schema(
       // required: true,
     },
     academicTerm: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "AcademicTerm",
-      // required: true,
+      type: String,
     },
   },
   { timestamps: true }
