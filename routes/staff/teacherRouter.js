f3d1fd9a18608c9698751b49214d656d42b3ac2c
Adam Lopez <adamf04@gmail.com>
2024-04-15 22:38:44 -0400

    adding API routes for profile, get single and update

--- routes/staff/teacherRouter.js
+++ routes/staff/teacherRouter.js
@@ -1,11 +1,24 @@
-const express = require("express");
-const isAdmin = require("../../middlewares/isAdmin");
-const isLogin = require("../../middlewares/isLogin");
-const { adminRegisterTeacher, loginTeacher, getAllTeachersAdmin } = require("../../controller/staff/teachersCtrl");
+const express        = require("express");
+const isAdmin        = require("../../middlewares/isAdmin");
+const isLogin        = require("../../middlewares/isLogin");
+const isTeacher      = require("../../middlewares/isTeacher");
+const isTeacherLogin = require("../../middlewares/isTeacherLogin");
+const {
+  adminRegisterTeacher,
+  loginTeacher,
+  getAllTeachersAdmin,
+  getTeacherByAdmin,
+  getTeacherProfile,
+  teacherUpdateProfile,
+} = require("../../controller/staff/teachersCtrl");
 const teachersRouter = express.Router();
 
 teachersRouter.post("/admin/register", isLogin, isAdmin, adminRegisterTeacher);
 teachersRouter.post("/login", loginTeacher);
 teachersRouter.get("/admin", isLogin, isAdmin, getAllTeachersAdmin);
+teachersRouter.get("/profile", isTeacherLogin, isTeacher, getTeacherProfile);
+
+teachersRouter.get("/:teacherID/admin", isLogin, isAdmin, getTeacherByAdmin);
+teachersRouter.put("/:teacherID/update", isTeacherLogin, isTeacher, teacherUpdateProfile);
 
 module.exports = teachersRouter;
