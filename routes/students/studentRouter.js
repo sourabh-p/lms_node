620b387f5e2ab5bb3e30d23bde11b42d412eaa30
Adam Lopez <adamf04@gmail.com>
2024-04-16 20:13:17 -0400

    creating the profile route and ensuring that only students that are logged in have have a role of 'student' are allowed to view the profile page.

--- routes/students/studentRouter.js
+++ routes/students/studentRouter.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
-const { adminRegisterStudent, loginStudent } = require("../../controller/students/studentsCtrl");
+const { adminRegisterStudent, loginStudent, getStudentProfile } = require("../../controller/students/studentsCtrl");
+const isStudent = require("../../middlewares/isStudent");
+const isStudentLogin = require("../../middlewares/isStudentLogin");
 
 const studentRouter = express.Router();
 
 studentRouter.post("/admin/register", isLogin, isAdmin, adminRegisterStudent);
 studentRouter.post("/login", loginStudent);
+studentRouter.get("/profile", isStudentLogin, isStudent, getStudentProfile);
 
 module.exports = studentRouter;
