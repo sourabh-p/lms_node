a11dd56939279aa363b7b2eeeb8bb886e62a87ec
Adam Lopez <adamf04@gmail.com>
2024-04-16 21:45:11 -0400

    Getting single student by admin

--- routes/students/studentRouter.js
+++ routes/students/studentRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
-const { adminRegisterStudent, loginStudent, getStudentProfile, getAllStudentsByAdmin } = require("../../controller/students/studentsCtrl");
+const { adminRegisterStudent, loginStudent, getStudentProfile, getAllStudentsByAdmin, getStudentByAdmin } = require("../../controller/students/studentsCtrl");
 const isStudent = require("../../middlewares/isStudent");
 const isStudentLogin = require("../../middlewares/isStudentLogin");
 
@@ -11,5 +11,6 @@ studentRouter.post("/admin/register", isLogin, isAdmin, adminRegisterStudent);
 studentRouter.post("/login", loginStudent);
 studentRouter.get("/profile", isStudentLogin, isStudent, getStudentProfile);
 studentRouter.get("/admin", isLogin, isAdmin, getAllStudentsByAdmin);
+studentRouter.get("/:studentID/admin", isLogin, isAdmin, getStudentByAdmin);
 
 module.exports = studentRouter;
