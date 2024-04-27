4d315c9f208bf2534d43ec01247182aa8f40833c
Adam Lopez <adamf04@gmail.com>
2024-04-27 15:46:00 -0400

    "Add route to get all exam results and update controller"
    This commit adds a new route to get all exam results in the academic controller and updates the corresponding route file.

--- controller/academics/examResults.js
+++ controller/academics/examResults.js
@@ -1,12 +1,25 @@
 const AsyncHandler = require("express-async-handler");
+const ExamResult = require("../../model/Academic/ExamResults");
+
 /**
  * @description Exam results check
  * @route       POST /api/v1/exam-results/:id/check
  * @access      Private Students Only
  */
-
-
-
 exports.checkExamResults = AsyncHandler(async  (req, res) => {
     res.json('checking results');
+});
+
+/**
+ * @description Get all exam results (name, id)
+ * @route       POST /api/v1/exam-results
+ * @access      Private Students Only
+ */
+exports.getExamResults = AsyncHandler( async ( req, res ) => {
+   const results = await ExamResult.find();
+   res.status(200).json({
+    status: "success",
+    message: "Exam results retrieved successfully",
+    data: results
+   });
 });
--- routes/academics/examResultsRoutes.js
+++ routes/academics/examResultsRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { checkExamResults } = require("../../controller/academics/examResults");
+const { checkExamResults, getExamResults } = require("../../controller/academics/examResults");
 
 const examResultRouter = express.Router();
 
 examResultRouter.get('/:id/check', checkExamResults);
+examResultRouter.get('/', getExamResults);
 
 module.exports = examResultRouter;
