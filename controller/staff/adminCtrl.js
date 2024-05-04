8e1c571f150ba7eacc6b3b00b9b1438e5e84e332
Adam Lopez <adamf04@gmail.com>
2024-05-03 23:05:19 -0400

    Formatting document. within the getAdminProfile method, I am populating data for the Admin to review.
    Also, commenting out the delete admin method. I don't want to delete admins just yet.

--- controller/staff/adminCtrl.js
+++ controller/staff/adminCtrl.js
@@ -11,25 +11,26 @@ const { hashPassword, isPassMatched } = require("../../utils/helpers");
  * @access      Private
  */
 exports.registerAdminCtrl = AsyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    // Check if admin already exists in the database
-    const adminFound = await Admin.findOne({ email });
+  // Check if admin already exists in the database
+  const adminFound = await Admin.findOne({ email });
 
-    if(adminFound) return res.status(401).json({ msg: "Email is already registered"});
+  if (adminFound)
+    return res.status(401).json({ msg: "Email is already registered" });
 
-    // register user
-    const user = await Admin.create({
-        name,
-        email,
-        password: await hashPassword(password)
-    });
+  // register user
+  const user = await Admin.create({
+    name,
+    email,
+    password: await hashPassword(password),
+  });
 
-    res.status(201).json({
-        status: "success",
-        data: user,
-        message: "Admin registered successfully. Glad you are here.",
-    }); 
+  res.status(201).json({
+    status: "success",
+    data: user,
+    message: "Admin registered successfully. Glad you are here.",
+  });
 });
 
 /**
@@ -38,28 +39,28 @@ exports.registerAdminCtrl = AsyncHandler(async (req, res) => {
  * @access      Private
  */
 exports.loginAdminCtrl = AsyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user                = await Admin.findOne({email})
+  const { email, password } = req.body;
+  const user = await Admin.findOne({ email });
 
-    if(!user) {
-        return res.json({
-            message: "Invalid login credentials. Please try again."
-        });
-    }
+  if (!user) {
+    return res.json({
+      message: "Invalid login credentials. Please try again.",
+    });
+  }
 
-    // verify password
-    const isMatched = await isPassMatched(password, user.password);
+  // verify password
+  const isMatched = await isPassMatched(password, user.password);
 
-    if (!isMatched) {
-        return res.json({
-            message: "Invalid login credentials. Please try again."
-        });
-    } else {
-        return res.json({
-            data: generateToken(user._id),
-            message: "Admin logged in successfully.  Welcome back!"
-        });
-    }
+  if (!isMatched) {
+    return res.json({
+      message: "Invalid login credentials. Please try again.",
+    });
+  } else {
+    return res.json({
+      data: generateToken(user._id),
+      message: "Admin logged in successfully.  Welcome back!",
+    });
+  }
 });
 /**
  * @description Get all admins
@@ -67,12 +68,12 @@ exports.loginAdminCtrl = AsyncHandler(async (req, res) => {
  * @access      Private
  */
 exports.getAdminsCtrl = AsyncHandler(async (req, res) => {
-    const admins = await Admin.find();
-    res.status(200).json({
-        status: "success",
-        message: "Admins fetched successfully",
-        data: admins,
-    });
+  const admins = await Admin.find();
+  res.status(200).json({
+    status: "success",
+    message: "Admins fetched successfully",
+    data: admins,
+  });
 });
 /**
  * @description Get Admin Profile
@@ -80,17 +81,25 @@ exports.getAdminsCtrl = AsyncHandler(async (req, res) => {
  * @access      Private
  */
 exports.getAdminProfileCtrl = AsyncHandler(async (req, res) => {
-    const admin = await Admin.findById(req.userAuth._id).select('-password -createdAt -updatedAt').populate("academicYear");
+  const admin = await Admin.findById(req.userAuth._id)
+    .select("-password -createdAt -updatedAt")
+    .populate("academicYear")
+    .populate("academicTerms")
+    .populate("programs")
+    .populate("yearGroups")
+    .populate("classLevels")
+    .populate("teachers")
+    .populate("students");
 
-    if (!admin) {
-        throw new Error("Admin not found")
-    } else {
-        res.status(200).json({
-            status: 'success',
-            data: admin,
-            message: "Admin Profile fetched successfully"
-        });
-    }
+  if (!admin) {
+    throw new Error("Admin not found");
+  } else {
+    res.status(200).json({
+      status: "success",
+      data: admin,
+      message: "Admin Profile fetched successfully",
+    });
+  }
 });
 /**
  * @description Update Admin
@@ -98,83 +107,90 @@ exports.getAdminProfileCtrl = AsyncHandler(async (req, res) => {
  * @access      Private
  */
 exports.updateAdminCtrl = AsyncHandler(async (req, res) => {
-    const {email, name, password} = req.body;
-    // if email is taken
-    const emailExists = await Admin.findOne({email});
-    if(emailExists) {
-        throw new Error("This email already exists");
-    }
+  const { email, name, password } = req.body;
+  // if email is taken
+  const emailExists = await Admin.findOne({ email });
+  if (emailExists) {
+    throw new Error("This email already exists");
+  }
 
-    // generate salt then hash the password
-    
+  // generate salt then hash the password
 
-    // check if user is updating password
-    if(password){
-        // update user
-        const admin = await Admin.findByIdAndUpdate(req.userAuth._id, {
-            email,
-            password: await hashPassword(password),
-            name,
-        }, {
-            new: true,
-            runValidators: true,
-        });
-        res.status(200).json({
-            success: "success",
-            data: admin,
-            message: "Admin profile updated successfully",
-        });
-    } else {
-        // update user email and name
-        const admin = await Admin.findByIdAndUpdate(req.userAuth._id, {
-            email,
-            name,
-        }, {
-            new: true,
-            runValidators: true,
-        });
-        res.status(200).json({
-            success: "success",
-            data: admin,
-            message: "Admin profile updated successfully",
-        });
-    }
+  // check if user is updating password
+  if (password) {
+    // update user
+    const admin = await Admin.findByIdAndUpdate(
+      req.userAuth._id,
+      {
+        email,
+        password: await hashPassword(password),
+        name,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      success: "success",
+      data: admin,
+      message: "Admin profile updated successfully",
+    });
+  } else {
+    // update user email and name
+    const admin = await Admin.findByIdAndUpdate(
+      req.userAuth._id,
+      {
+        email,
+        name,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      success: "success",
+      data: admin,
+      message: "Admin profile updated successfully",
+    });
+  }
 });
 /**
  * @description Delete Admin
  * @route       DELETE /api/v1/admins/:id
  * @access      Private
  */
-exports.deleteAdminCtrl = (req, res) => {
-    try {
-        res.status(201).json({
-            status: 'success',
-            data: 'Admin has been Deleted successfully'
-        });
-    } catch (error) {
-        res.json({
-            status: "failed",
-            error: error.message
-        })
-    }
-};
+// exports.deleteAdminCtrl = (req, res) => {
+//   try {
+//     res.status(201).json({
+//       status: "success",
+//       data: "Admin has been Deleted successfully",
+//     });
+//   } catch (error) {
+//     res.json({
+//       status: "failed",
+//       error: error.message,
+//     });
+//   }
+// };
 /**
  * @description Admin suspends a teacher
  * @route       PUT /api/v1/admins/suspend/teacher:id
  * @access      Private
  */
 exports.adminSuspendTeacherCtrl = (req, res) => {
-    try {
-        res.status(201).json({
-            status: 'success',
-            data: 'Admin has Suspended teacher successfully'
-        });
-    } catch (error) {
-        res.json({
-            status: "failed",
-            error: error.message
-        })
-    }
+  try {
+    res.status(201).json({
+      status: "success",
+      data: "Admin has Suspended teacher successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      error: error.message,
+    });
+  }
 };
 /**
  * @description Admin unsuspends a teacher
@@ -182,17 +198,17 @@ exports.adminSuspendTeacherCtrl = (req, res) => {
  * @access      Private
  */
 exports.adminUnsuspendteacherCtrl = (req, res) => {
-    try {
-        res.status(201).json({
-            status: 'success',
-            data: 'Admin has Unsuspended teacher successfully'
-        });
-    } catch (error) {
-        res.json({
-            status: "failed",
-            error: error.message
-        })
-    }
+  try {
+    res.status(201).json({
+      status: "success",
+      data: "Admin has Unsuspended teacher successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      error: error.message,
+    });
+  }
 };
 /**
  * @description Admin withdrawl a teacher
@@ -200,17 +216,17 @@ exports.adminUnsuspendteacherCtrl = (req, res) => {
  * @access      Private
  */
 exports.adminWithdrawTeacherCtrl = (req, res) => {
-    try {
-        res.status(201).json({
-            status: 'success',
-            data: 'Admin has withdrawn teacher successfully'
-        });
-    } catch (error) {
-        res.json({
-            status: "failed",
-            error: error.message
-        })
-    }
+  try {
+    res.status(201).json({
+      status: "success",
+      data: "Admin has withdrawn teacher successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      error: error.message,
+    });
+  }
 };
 /**
  * @description Admin Unwithdrawl a teacher
@@ -218,17 +234,17 @@ exports.adminWithdrawTeacherCtrl = (req, res) => {
  * @access      Private
  */
 exports.adminUnwithdrawTeacherCtrl = (req, res) => {
-    try {
-        res.status(201).json({
-            status: 'success',
-            data: 'Admin has Unwithdrawn teacher successfully'
-        });
-    } catch (error) {
-        res.json({
-            status: "failed",
-            error: error.message
-        })
-    }
+  try {
+    res.status(201).json({
+      status: "success",
+      data: "Admin has Unwithdrawn teacher successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      error: error.message,
+    });
+  }
 };
 /**
  * @description Admin Publish Exam Results
@@ -236,17 +252,17 @@ exports.adminUnwithdrawTeacherCtrl = (req, res) => {
  * @access      Private
  */
 exports.adminPublishResultsCtrl = (req, res) => {
-    try {
-        res.status(201).json({
-            status: 'success',
-            data: 'Admin has published exam result(s) successfully'
-        });
-    } catch (error) {
-        res.json({
-            status: "failed",
-            error: error.message
-        })
-    }
+  try {
+    res.status(201).json({
+      status: "success",
+      data: "Admin has published exam result(s) successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      error: error.message,
+    });
+  }
 };
 /**
  * @description Admin Unpublish Exam Results
@@ -254,15 +270,15 @@ exports.adminPublishResultsCtrl = (req, res) => {
  * @access      Private
  */
 exports.adminUnpublishResultsCtrl = (req, res) => {
-    try {
-        res.status(201).json({
-            status: 'success',
-            data: 'Admin has Unpublished exam result(s) successfully'
-        });
-    } catch (error) {
-        res.json({
-            status: "failed",
-            error: error.message
-        })
-    }
+  try {
+    res.status(201).json({
+      status: "success",
+      data: "Admin has Unpublished exam result(s) successfully",
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      error: error.message,
+    });
+  }
 };
