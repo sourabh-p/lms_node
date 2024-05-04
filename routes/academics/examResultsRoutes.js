dba922015ccd2019f485768ffed3c97fbb93d5ac
Adam Lopez <adamf04@gmail.com>
2024-05-03 23:07:17 -0400

    Adding the routing for the adminToggleExamResult ensuring that only admins that are logged in can access this route using middlewares.

--- routes/academics/examResultsRoutes.js
+++ routes/academics/examResultsRoutes.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const { checkExamResults, getExamResults } = require("../../controller/academics/examResults");
+const {
+  checkExamResults,
+  getExamResults,
+  adminToggleExamResult,
+} = require("../../controller/academics/examResults");
 const isStudent = require("../../middlewares/isStudent");
 const isStudentLogin = require("../../middlewares/isStudentLogin");
+const isAdmin = require("../../middlewares/isAdmin");
+const isLogin = require("../../middlewares/isLogin");
 
 const examResultRouter = express.Router();
 
-examResultRouter.get('/:id/check', isStudentLogin, isStudent, checkExamResults);
-examResultRouter.get('/', isStudentLogin, isStudent, getExamResults);
+examResultRouter.get("/:id/check", isStudentLogin, isStudent, checkExamResults);
+examResultRouter.get("/", isStudentLogin, isStudent, getExamResults);
+
+examResultRouter.put(
+  "/:id/admin-toggle-publish",
+  isLogin,
+  isAdmin,
+  adminToggleExamResult
+);
 
 module.exports = examResultRouter;
