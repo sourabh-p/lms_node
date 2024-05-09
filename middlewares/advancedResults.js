51f15469d13e692e106d9a496a78b458a561fc67
Adam Lopez <adamf04@gmail.com>
2024-05-09 11:23:57 -0400

    Creating a high order function that returns another function within it that will take the model name and the populate data and pass it to another function in order to retrieve the req, res, and next components of the model objects being passed to it.

---
+++ middlewares/advancedResults.js
@@ -0,0 +1,10 @@
+// model,populate
+
+
+// high order function that returns another function
+const advancedResults = (model, populate) => {
+    // since req is here, we can use queries
+    return (req, res, next) => {
+
+    }
+}
