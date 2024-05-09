0be7acc6f42be2e88fa8d975f9bc1398b1beb376
Adam Lopez <adamf04@gmail.com>
2024-05-09 16:17:47 -0400

    Applying the advancedResults middleware to the Questions controller and router. Passed the Questions model into the middleware and tested on postman. Pagination is returning as expected.

--- routes/academics/questionRoutes.js
+++ routes/academics/questionRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const isTeacher = require("../../middlewares/isTeacher");
 const isTeacherLogin = require("../../middlewares/isTeacherLogin");
 const { createQuestion, getQuestions, getQuestion, updateQuestion } = require("../../controller/academics/questionsCtrl");
+const advancedResults = require("../../middlewares/advancedResults");
+const Question = require("../../model/Academic/Questions");
 
 const questionsRouter = express.Router();
 
-questionsRouter.get('/', isTeacherLogin, isTeacher, getQuestions);
+questionsRouter.get('/', isTeacherLogin, isTeacher, advancedResults(Question) ,getQuestions);
 questionsRouter.get('/:id', isTeacherLogin, isTeacher, getQuestion);
 questionsRouter.post('/:examID', isTeacherLogin, isTeacher, createQuestion);
 questionsRouter.put('/:id', isTeacherLogin, isTeacher, updateQuestion);
