f20f481fb2c7a2f2845c3886732c113bf2537ee3
Adam Lopez <adamf04@gmail.com>
2024-05-09 14:20:54 -0400

    Applying the advancedResults middleware to the Program controller and router. Passed the Program model into the middleware and tested on postman. Pagination is returning as expected.

--- routes/academics/program.js
+++ routes/academics/program.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { createProgram, getPrograms, getSingleProgram, updateProgram, deleteProgram } = require("../../controller/academics/programsCtrl");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
+const advancedResults = require("../../middlewares/advancedResults");
+const Program = require("../../model/Academic/Program");
 
 const programRouter = express.Router();
 
@@ -11,7 +13,7 @@ const programRouter = express.Router();
 programRouter
   .route("/")
   .post(isLogin, isAdmin, createProgram)
-  .get(isLogin, isAdmin, getPrograms);
+  .get(isLogin, isAdmin, advancedResults(Program),getPrograms);
 
 programRouter
   .route("/:id")
