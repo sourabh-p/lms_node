4932fc555e76c3f17fae7ea9380e2f5ee73bf8ab
Adam Lopez <adamf04@gmail.com>
2024-05-09 14:23:14 -0400

    Applying the advancedResults middleware to the Subject controller and router. Passed the Subject model into the middleware and tested on postman. Pagination is returning as expected.

--- routes/academics/subject.js
+++ routes/academics/subject.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { createSubject, getSubjects, getSubject, updateSubject, deleteSubject } = require("../../controller/academics/subjectCtrl");
 const isAdmin = require("../../middlewares/isAdmin");
 const isLogin = require("../../middlewares/isLogin");
+const advancedResults = require("../../middlewares/advancedResults");
+const Subject = require("../../model/Academic/Subject");
 
 const subjectRouter = express.Router();
 
@@ -9,7 +11,7 @@ const subjectRouter = express.Router();
  * updated chained routes
  */
 subjectRouter.post('/:programID', isLogin, isAdmin, createSubject);
-subjectRouter.get('/', isLogin, isAdmin, getSubjects);
+subjectRouter.get('/', isLogin, isAdmin, advancedResults(Subject), getSubjects);
 subjectRouter.get('/:id', isLogin, isAdmin, getSubject);
 subjectRouter.put('/:id', isLogin, isAdmin, updateSubject);
 subjectRouter.delete('/:id', isLogin, isAdmin, deleteSubject);
