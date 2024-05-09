3c02c45b2e1449e2c44d011bf67ae0079e266c8b
Adam Lopez <adamf04@gmail.com>
2024-05-09 11:59:41 -0400

    For the controller I am allowing the middleware to do most of the logic and only returning a successful response with the results from the middleware `advancedResults.js`

--- controller/staff/teachersCtrl.js
+++ controller/staff/teachersCtrl.js
@@ -73,61 +73,9 @@ exports.loginTeacher = expressAsyncHandler(async  (req, res)=>{
  * @access      Private admin only
  */
 exports.getAllTeachersAdmin = expressAsyncHandler( async(req, res) => {
-    console.log(req.res);  
-    let TeachersQuery = Teacher.find(); // return all data via `query`, then do pagination using mongoose
-    /**
-     * query string - additional data passed to url (optional - controller will still run) 
-     * params /:id any value that comes after id and must match db
-     * 
-     */
 
-    // If the query string has the name property, search for the name
-    if( req.query.name ){ 
-        // filtering/searching
-        TeachersQuery = TeachersQuery.find({
-            name: {$regex: req.query.name, $options: "i"},
-        });
-    }
-
-    /**
-     * Convert query strings to numbers
-     */
-    const page  = Number(req.query.page) || 1; // using number constructor, look for query params 'page'
-    const limit = Number(req.query.limit) || 2; // using number constructor, look for query params 'limit'
-    const skip  = (page - 1) * limit; // page minus 1 times the limit
-    const total = await Teacher.countDocuments(); // get total records
-
-    const startIndex = (page - 1) * limit; // start index of the current page
-    const endIndex = page * limit; // end index of the current page
-
-    // pagination results
-    const pagination = {};
-    // add next
-    if(endIndex < total) {
-        pagination.next = {
-            page: page + 1,
-            limit
-        }
-    }
-    // add prev
-    if(startIndex > 0) {
-        pagination.prev = {
-            page: page - 1,
-            limit
-        }
-    }
-
-    res.status(200).json(res.myData);
-
-    // const teachers = await TeachersQuery.find().skip(skip).limit(limit);
-    // res.status(200).json({
-    //     total,
-    //     pagination,
-    //     results: teachers.length,
-    //     status: "success",
-    //     message: "Teachers fetched successfully",
-    //     data: teachers,
-    // });
+    res.status(200).json(res.results);
+    
 });
 
 /**
