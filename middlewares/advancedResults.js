7eea5dab0f26617d1d41368634407baa487bbbda
Adam Lopez <adamf04@gmail.com>
2024-05-09 12:17:53 -0400

    Changed some of the names to make this middleware make more sense. Removed old comments as well, I can reference them in previous commits if need be. This middleware is using a dynamic way of passing a model and any data associated with the IDs of the model to populate data on the fly. This is extremely fast and helpful.

--- middlewares/advancedResults.js
+++ middlewares/advancedResults.js
@@ -4,32 +4,27 @@
 const advancedResults = (model, populate) => {
   // since req is here, we can use queries
   return async (req, res, next) => {
-    console.log(req.res);
-    let TeachersQuery = model.find(); // return all data via `query`, then do pagination using mongoose
-    /**
-     * query string - additional data passed to url (optional - controller will still run)
-     * params /:id any value that comes after id and must match db
-     *
-     */
+    // console.log(req.res);
+    let ModelQuery   = model.find(); // return all data via `query`, then do pagination using mongoose
+    const page       = Number(req.query.page) || 1; // using number constructor, look for query params 'page'
+    const limit      = Number(req.query.limit) || 2; // using number constructor, look for query params 'limit'
+    const skip       = (page - 1) * limit; // page minus 1 times the limit
+    const total      = await model.countDocuments(); // get total records
+    const startIndex = (page - 1) * limit; // start index of the current page
+    const endIndex   = page * limit; // end index of the current page
+
+    // populate
+    if(populate) {
+        ModelQuery = ModelQuery.populate(populate);
+    }
 
     // If the query string has the name property, search for the name
     if (req.query.name) {
-      // filtering/searching
-      TeachersQuery = TeachersQuery.find({
+      // filtering/searching by name
+      ModelQuery = ModelQuery.find({
         name: { $regex: req.query.name, $options: "i" },
       });
     }
-
-    /**
-     * Convert query strings to numbers
-     */
-    const page = Number(req.query.page) || 1; // using number constructor, look for query params 'page'
-    const limit = Number(req.query.limit) || 2; // using number constructor, look for query params 'limit'
-    const skip = (page - 1) * limit; // page minus 1 times the limit
-    const total = await model.countDocuments(); // get total records
-
-    const startIndex = (page - 1) * limit; // start index of the current page
-    const endIndex = page * limit; // end index of the current page
 
     // pagination results
     const pagination = {};
@@ -48,15 +43,15 @@ const advancedResults = (model, populate) => {
       };
     }
 
-    const teachers = await TeachersQuery.find().skip(skip).limit(limit);
+    const AdvancedResults = await ModelQuery.find().skip(skip).limit(limit);
 
     res.results = {
         total,
         pagination,
-        results: teachers.length,
+        results: AdvancedResults.length,
         status: "success",
-        message: "Teachers fetched successfully",
-        data: teachers,
+        message: "Advanced Results fetched successfully",
+        data: AdvancedResults,
     }
 
     next();
